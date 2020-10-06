e2bdbc6fc95b72d6497ba11628b52740fcdd1a34
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-06 21:54:00 +0530

    Computation1 performed

--- arithmeticOperations.sh
+++ arithmeticOperations.sh
@@ -3,8 +3,13 @@
 echo "Arithmetic computation and sorting"
 
 #uc1 take three inputs
+
 read -p "enter a:" a
 read -p "enter b:" b
 read -p "enter c:" c
 
+#uc2 Computation1
+
+Computation1=$(( a+b*c ))
+echo "Computation1=" $Computation1
 
